d69dd0ac30ac813ea2439cde265c76c376c96c1d
Bryan Zheng <ashuh223@gmail.com>
2021-05-15 09:49:12 +0800

    Revert "feat: Implement collision checking"
    This reverts commit 2e5d40daf1462cc5a0e1c2dd7317c6674297eab3.

--- collision_warning_system/include/collision_warning_system/bicycle_model.h
+++ collision_warning_system/include/collision_warning_system/bicycle_model.h
@@ -1,12 +1,7 @@
 #ifndef COLLISION_WARNING_SYSTEM_BICYCLE_MODEL_H
 #define COLLISION_WARNING_SYSTEM_BICYCLE_MODEL_H
 
-#include <string>
 #include <vector>
-
-#include <nav_msgs/Path.h>
-#include <geometry_msgs/Pose.h>
-
 #include "collision_warning_system/bicycle_state.h"
 
 namespace f1tenth_racecar
@@ -19,16 +14,12 @@ private:
   double wheelbase_;
 
   BicycleState propagateState(const BicycleState state, const double delta_t);
-  nav_msgs::Path bicycleStatesToPath(const std::vector<BicycleState> states, std::string frame_id);
-  geometry_msgs::PoseStamped bicycleStateToPoseStamped(const BicycleState state, std::string frame_id);
 
 public:
   BicycleModel();
   explicit BicycleModel(const double wheel_base);
 
-  nav_msgs::Path projectTrajectory(const geometry_msgs::Pose initial_pose, const std::string frame_id,
-                                   const double velocity, const double steering_angle, const double delta_t,
-                                   const double steps);
+  std::vector<BicycleState> projectTrajectory(const BicycleState state, const double delta_t, const double steps);
 };
 }  // namespace safety
 }  // namespace f1tenth_racecar
