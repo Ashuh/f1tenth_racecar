9159ec926416019bb00c8c071f11910651218c24
Bryan Zheng <ashuh223@gmail.com>
2021-05-17 10:25:53 +0800

    feat(DS4): Send feedback to DS4 Driver node
    Map DS4 battery percentage to color fading from red to yellow to green
    
    Wait for connection before exiting constructor and rumble upon success

--- ds4_controller/src/ds4_controller.cpp
+++ ds4_controller/src/ds4_controller.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <string>
 
 #include <ros/ros.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 
+#include "ds4_driver/Feedback.h"
 #include "ds4_driver/Status.h"
 #include "ds4_controller/ds4_controller.h"
 
@@ -15,19 +17,27 @@ DS4Controller::DS4Controller()
   ros::NodeHandle private_nh("~");
 
   std::string status_topic;
+  std::string feedback_topic;
   std::string drive_topic;
 
   ROS_ASSERT(private_nh.getParam("status_topic", status_topic));
+  ROS_ASSERT(private_nh.getParam("feedback_topic", feedback_topic));
   ROS_ASSERT(private_nh.getParam("drive_topic", drive_topic));
   ROS_ASSERT(private_nh.getParam("max_steering_angle", max_steering_angle_));
   ROS_ASSERT(private_nh.getParam("max_speed", max_speed_));
 
   status_sub_ = nh_.subscribe(status_topic, 1, &DS4Controller::statusCallback, this);
   drive_pub_ = nh_.advertise<ackermann_msgs::AckermannDriveStamped>(drive_topic, 1);
+  feedback_pub_ = nh_.advertise<ds4_driver::Feedback>(feedback_topic, 1);
+
+  waitForConnection();
+
+  timer_ = nh_.createTimer(ros::Duration(0.1), &DS4Controller::publishFeedbackMsg, this);
 }
 
 void DS4Controller::statusCallback(const ds4_driver::Status status_msg)
 {
+  battery_percentage_ = status_msg.battery_percentage;
   publishDriveMsg(status_msg.axis_right_y, status_msg.axis_left_x);
 }
 
@@ -39,6 +49,40 @@ void DS4Controller::publishDriveMsg(const double throttle_axis, const double ste
   drive_msg.drive.steering_angle = steering_axis * max_steering_angle_;
 
   drive_pub_.publish(drive_msg);
+}
+
+void DS4Controller::publishFeedbackMsg(const ros::TimerEvent& timer_event)
+{
+  ds4_driver::Feedback feedback_msg;
+  feedback_msg.set_led = true;
+  batteryToRGB(feedback_msg.led_r, feedback_msg.led_g, feedback_msg.led_b);
+
+  feedback_pub_.publish(feedback_msg);
+}
+
+void DS4Controller::waitForConnection()
+{
+  while (status_sub_.getNumPublishers() == 0)
+  {
+    ROS_WARN_THROTTLE(1, "[DS4 Controller] Waiting for connection");
+  }
+
+  ROS_INFO("[DS4 Controller] Connection success");
+
+  ds4_driver::Feedback feedback_msg;
+  feedback_msg.set_rumble = true;
+  feedback_msg.rumble_duration = 0.5;
+  feedback_msg.rumble_small = 1;
+  feedback_pub_.publish(feedback_msg);
+
+  ros::Duration(0.5).sleep();
+}
+
+void DS4Controller::batteryToRGB(float& r, float& g, float& b)
+{
+  r = std::min(2.0 * (1 - battery_percentage_), 1.0);
+  g = std::min(2.0 * battery_percentage_, 1.0);
+  b = 0;
 }
 }  // namespace utils
 }  // namespace f1tenth_racecar
