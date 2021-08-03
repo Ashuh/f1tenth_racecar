7878f875c41f782b35109f63b61b22a30f4b2498
Bryan Zheng <ashuh223@gmail.com>
2021-08-03 16:29:14 +0800

    Update  main node logic

--- local_planner/include/local_planner/local_planner.h
+++ local_planner/include/local_planner/local_planner.h
@@ -1,32 +1,33 @@
 #ifndef LOCAL_PLANNER_LOCAL_PLANNER_H
 #define LOCAL_PLANNER_LOCAL_PLANNER_H
 
+#include <string>
 #include <vector>
 
 #include <ros/ros.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
-#include <geometry_msgs/Pose2D.h>
+#include <dynamic_reconfigure/server.h>
 #include <grid_map_msgs/GridMap.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
 
-#include "local_planner/path.h"
+#include "costmap_generator/collision_checker.h"
 #include "local_planner/trajectory.h"
-#include "local_planner/cubic_spiral_optimizer.h"
-#include "local_planner/velocity_profile_generator.h"
-#include "local_planner/trajectory_evaluator.h"
+#include "local_planner/reference_trajectory_generator.h"
+#include "local_planner/tracking_trajectory_generator.h"
+#include "local_planner/LocalPlannerConfig.h"
 
 class LocalPlanner
 {
+  using RTG = ReferenceTrajectoryGenerator;
+  using TTG = TrackingTrajectoryGenerator;
+
 private:
   ros::NodeHandle nh_;
-
   ros::Timer timer_;
-
-  tf2_ros::Buffer tf_buffer_;
-  tf2_ros::TransformListener tf_listener_;
 
   ros::Subscriber global_path_sub_;
   ros::Subscriber odom_sub_;
@@ -35,12 +36,19 @@ private:
 
   ros::Publisher local_path_pub_;
   ros::Publisher viz_pub_;
+  ros::Publisher inflated_costmap_pub_;
 
-  std::unique_ptr<CubicSpiralOptimizer> opt_;
-  std::unique_ptr<VelocityProfileGenerator> velocity_gen_;
-  std::unique_ptr<TrajectoryEvaluator> traj_eval_;
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 
-  nav_msgs::Path global_path_;
+  dynamic_reconfigure::Server<local_planner::LocalPlannerConfig> server_;
+  dynamic_reconfigure::Server<local_planner::LocalPlannerConfig>::CallbackType f_;
+
+  std::unique_ptr<ReferenceTrajectoryGenerator> ref_traj_gen_ptr_;
+  std::unique_ptr<TrackingTrajectoryGenerator> track_traj_gen_ptr_;
+  std::shared_ptr<CollisionChecker> collision_checker_ptr_;
+  std::shared_ptr<visualization_msgs::MarkerArray> viz_ptr_;
+
   nav_msgs::Odometry latest_odom_;
   double current_steering_angle_;
 
@@ -52,24 +60,15 @@ private:
 
   void timerCallback(const ros::TimerEvent& timer_event);
 
-  void globalPathCallback(const nav_msgs::Path& path_msg);
+  void globalPathCallback(const nav_msgs::Path::ConstPtr& path_msg);
 
-  void odomCallback(const nav_msgs::Odometry& odom_msg);
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg);
 
-  void driveCallback(const ackermann_msgs::AckermannDriveStamped& drive_msg);
+  void driveCallback(const ackermann_msgs::AckermannDriveStamped::ConstPtr& drive_msg);
 
   void costmapCallback(const grid_map_msgs::GridMap::ConstPtr& costmap_msg);
 
-  void visualizePaths(const std::vector<Trajectory>& trajectories, const std::vector<double>& costs);
-
-  int getNearestWaypointId();
-
-  geometry_msgs::Pose2D getReferenceGoal();
-
-  geometry_msgs::Pose2D generateOffsetGoal(const geometry_msgs::Pose2D& reference_goal, const double lateral_offset);
-
-  // std::vector<geometry_msgs::Pose2D> generateGoals(const geometry_msgs::Pose2D& reference_goal, const int num_goals,
-  //                                                  const double lateral_spacing);
+  void configCallback(local_planner::LocalPlannerConfig& config, uint32_t level);
 
   nav_msgs::Path trajectoryToPathMsg(const Trajectory& trajectory);
 
--- local_planner/src/local_planner.cpp
+++ local_planner/src/local_planner.cpp
@@ -1,300 +1,209 @@
-#include <algorithm>
-#include <limits>
 #include <string>
 #include <vector>
 
 #include <ros/ros.h>
-#include <geometry_msgs/Pose2D.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
+#include <dynamic_reconfigure/server.h>
 #include <grid_map_msgs/GridMap.h>
 #include <nav_msgs/Path.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
 
-#include "local_planner/path.h"
+#include "costmap_generator/collision_checker.h"
+#include "local_planner/lattice.h"
+#include "local_planner/reference_trajectory_generator.h"
+#include "local_planner/tracking_trajectory_generator.h"
 #include "local_planner/trajectory.h"
-#include "local_planner/cubic_spiral_optimizer.h"
-#include "local_planner/velocity_profile_generator.h"
-#include "local_planner/trajectory_evaluator.h"
 #include "local_planner/local_planner.h"
+#include "local_planner/LocalPlannerConfig.h"
 
 LocalPlanner::LocalPlanner() : tf_listener_(tf_buffer_)
 {
   ros::NodeHandle private_nh("~");
 
-  std::string costmap_topic = "costmap";
-  std::string global_path_topic = "path/global";
-  std::string local_path_topic = "path/local";
-  std::string odom_topic = "odom";
-  std::string viz_topic = "viz/local_planner";
+  /* ------------------------ Collision Checking Params ----------------------- */
 
-  double max_lat_acc = 1;
-  double max_long_acc = 1;
-  double max_steering_angle = 0.4;
+  std::vector<double> circle_offsets;
+  double circle_radius;
 
-  // ROS_ASSERT(private_nh.getParam("global_path_topic", global_path_topic));
-  // ROS_ASSERT(private_nh.getParam("local_path_topic", local_path_topic));
-  // ROS_ASSERT(private_nh.getParam("odom_topic", odom_topic));
-  // ROS_ASSERT(private_nh.getParam("viz_topic", viz_topic));
+  private_nh.param("circle_offsets", circle_offsets, std::vector<double>{ 0.1, 0.3 });
+  private_nh.param("circle_radius", circle_radius, 0.2);
 
-  // ROS_ASSERT(private_nh.getParam("max_lat_acc", max_lat_acc));
-  // ROS_ASSERT(private_nh.getParam("max_long_acc", max_long_acc));
-  // ROS_ASSERT(private_nh.getParam("wheelbase", wheelbase));
-  // ROS_ASSERT(private_nh.getParam("max_steering_angle", max_steering_angle));
-  // ROS_ASSERT(private_nh.getParam("num_paths", num_paths_));
-  // ROS_ASSERT(private_nh.getParam("path_offset", path_offset_));
-  // ROS_ASSERT(private_nh.getParam("look_ahead_time", look_ahead_time_));
-  // ROS_ASSERT(private_nh.getParam("min_look_ahead_dist", min_look_ahead_dist_));
+  for (auto& o : circle_offsets)
+  {
+    ROS_INFO_STREAM(o);
+  }
+
+  /* ----------------------- Refernce Trajectory Params ----------------------- */
+
+  int lattice_num_layers;
+  int lattice_num_lateral_samples_per_side;
+  double lattice_longitudinal_spacing;
+  double lattice_lateral_spacing;
+  double lattice_k_length;
+
+  double ref_max_speed;
+  double ref_max_lat_acc;
+  double ref_max_lon_acc;
+  double ref_max_lon_dec;
+
+  private_nh.param("lattice_num_layers", lattice_num_layers, 8);
+  private_nh.param("lattice_num_lateral_samples_per_side", lattice_num_lateral_samples_per_side, 8);
+  private_nh.param("lattice_longitudinal_spacing", lattice_longitudinal_spacing, 2.0);
+  private_nh.param("lattice_lateral_spacing", lattice_lateral_spacing, 0.05);
+  private_nh.param("lattice_k_length", lattice_k_length, 0.5);
+
+  private_nh.param("ref_max_speed", ref_max_speed, 5.0);
+  private_nh.param("ref_max_lat_acc", ref_max_lat_acc, 1.0);
+  private_nh.param("ref_max_lon_acc", ref_max_lon_acc, 1.0);
+  private_nh.param("ref_max_lon_dec", ref_max_lon_dec, 1.0);
+
+  /* ----------------------- Tracking Trajectory Params ----------------------- */
+
+  int track_num_paths;
+  double wheelbase;
+  double max_steering_angle;
+  double track_path_lateral_spacing;
+  double track_look_ahead_time;
+
+  private_nh.param("track_num_paths", track_num_paths, 9);
+  private_nh.param("wheelbase", wheelbase, 0.3);
+  private_nh.param("max_steering_angle", max_steering_angle, 0.4);
+  private_nh.param("track_path_lateral_spacing", track_path_lateral_spacing, 0.05);
+  private_nh.param("track_look_ahead_time", track_look_ahead_time, 0.5);
+
+  /* --------------------------------- Topics --------------------------------- */
+
+  std::string costmap_topic;
+  std::string global_path_topic;
+  std::string local_path_topic;
+  std::string odom_topic;
+  std::string inflated_costmap_topic;
+  std::string viz_topic;
+
+  private_nh.param("costmap_topic", costmap_topic, std::string("costmap"));
+  private_nh.param("global_path_topic", global_path_topic, std::string("path/global"));
+  private_nh.param("local_path_topic", local_path_topic, std::string("path/local"));
+  private_nh.param("odom_topic", odom_topic, std::string("odom"));
+  private_nh.param("inflated_costmap_topic", inflated_costmap_topic, std::string("inflated_costmap"));
+  private_nh.param("viz_topic", viz_topic, std::string("viz/local_planner"));
+
+  /* ------------------------ Subscribers & Publishers ------------------------ */
 
   global_path_sub_ = nh_.subscribe(global_path_topic, 1, &LocalPlanner::globalPathCallback, this);
   odom_sub_ = nh_.subscribe(odom_topic, 1, &LocalPlanner::odomCallback, this);
   drive_sub_ = nh_.subscribe("drive", 1, &LocalPlanner::driveCallback, this);
   costmap_sub_ = nh_.subscribe(costmap_topic, 1, &LocalPlanner::costmapCallback, this);
+  timer_ = nh_.createTimer(ros::Duration(0.1), &LocalPlanner::timerCallback, this);
 
   local_path_pub_ = nh_.advertise<nav_msgs::Path>(local_path_topic, 1);
   viz_pub_ = nh_.advertise<visualization_msgs::MarkerArray>(viz_topic, 1);
+  inflated_costmap_pub_ = nh_.advertise<nav_msgs::OccupancyGrid>(inflated_costmap_topic, 1);
 
-  timer_ = nh_.createTimer(ros::Duration(0.1), &LocalPlanner::timerCallback, this);
+  /* ---------------------------- Shared Resources ---------------------------- */
 
-  wheelbase_ = 0.3;
-  num_paths_ = 10;
-  path_offset_ = 0.2;
-  look_ahead_time_ = 2.0;
-  min_look_ahead_dist_ = 2.0;
+  viz_ptr_ = std::make_shared<visualization_msgs::MarkerArray>();
+  collision_checker_ptr_ = std::make_shared<CollisionChecker>(circle_offsets, circle_radius);
 
-  opt_ = std::make_unique<CubicSpiralOptimizer>(tan(max_steering_angle) / wheelbase_);
-  velocity_gen_ = std::make_unique<VelocityProfileGenerator>(max_lat_acc, max_long_acc);
-  traj_eval_ = std::make_unique<TrajectoryEvaluator>();
+  /* --------------------- Reference Trajectory Generator --------------------- */
+
+  Lattice::Generator::Pattern lattice_pattern(lattice_num_layers, lattice_longitudinal_spacing,
+                                              lattice_num_lateral_samples_per_side, lattice_lateral_spacing);
+  Lattice::Generator lat_gen(lattice_pattern, lattice_k_length, collision_checker_ptr_);
+  RTG::VelocityConstraints ref_vel_constraints(ref_max_speed, ref_max_lat_acc, ref_max_lon_acc, ref_max_lon_dec);
+
+  ref_traj_gen_ptr_ = std::make_unique<RTG>(lat_gen, ref_vel_constraints, viz_ptr_);
+
+  /* ---------------------- Tracking Trajectory Generator --------------------- */
+
+  TTG::SamplingPattern tt_pattern(track_num_paths, track_path_lateral_spacing, track_look_ahead_time);
+  double max_curvature = tan(max_steering_angle) / wheelbase;
+
+  track_traj_gen_ptr_ = std::make_unique<TTG>(tt_pattern, max_curvature, collision_checker_ptr_, viz_ptr_);
+
+  /* --------------------------- Dynamic Reconfigure -------------------------- */
+
+  f_ = boost::bind(&LocalPlanner::configCallback, this, _1, _2);
+  server_.setCallback(f_);
 }
 
 void LocalPlanner::timerCallback(const ros::TimerEvent& timer_event)
 {
-  geometry_msgs::Pose2D reference_goal;
-
   try
   {
-    reference_goal = getReferenceGoal();
+    /* --------------------- Reference Trajectory Generator --------------------- */
+
+    ros::Time ref_begin = ros::Time::now();
+    Trajectory reference_trajectory = ref_traj_gen_ptr_->generateReferenceTrajectory(latest_odom_.pose.pose);
+    double ref_time = (ros::Time::now() - ref_begin).toSec();
+
+    /* ---------------------- Tracking Trajectory Generator --------------------- */
+
+    ros::Time track_begin = ros::Time::now();
+    double current_curvature = tan(current_steering_angle_) / wheelbase_;
+    Trajectory tracking_trajectory =
+        track_traj_gen_ptr_->generateTrackingTrajectory(reference_trajectory, current_curvature);
+    double track_time = (ros::Time::now() - track_begin).toSec();
+
+    ROS_INFO("[Local Planner] Time: [Total] %.3fs [Ref] %.3fs [Track] %.3fs", ref_time + track_time, ref_time,
+             track_time);
+
+    /* --------------------------- Publish Trajectory --------------------------- */
+
+    nav_msgs::Path msg = trajectoryToPathMsg(tracking_trajectory);  // use standard path msg for now
+    local_path_pub_.publish(msg);
   }
   catch (const std::exception& ex)
   {
     ROS_ERROR("[Local Planner] %s", ex.what());
-    return;
   }
 
-  double current_curvature = tan(current_steering_angle_) / wheelbase_;
-
-  std::vector<Trajectory> trajectories;
-  std::vector<double> costs;
-
-  ros::Time begin = ros::Time::now();
-
-  for (int i = 0; i < num_paths_; ++i)
-  {
-    double goal_offset = (i - num_paths_ / 2) * path_offset_;
-
-    geometry_msgs::Pose2D goal = generateOffsetGoal(reference_goal, goal_offset);
-    Path path = opt_->generateCubicSpiralPath(current_curvature, 0.0, goal.x, goal.y, goal.theta, 10);
-    Trajectory trajectory = velocity_gen_->generateVelocityProfile(path, latest_odom_.twist.twist.linear.x, 5);
-    double traj_cost = traj_eval_->evaluateTrajectory(trajectory, goal_offset);
-
-    trajectories.push_back(trajectory);
-    costs.push_back(traj_cost);
-  }
-
-  ros::Time end = ros::Time::now();
-  ROS_INFO_STREAM("PLANNING TIME: " << (end - begin).toSec());
-
-  int best_traj_id = std::min_element(costs.begin(), costs.end()) - costs.begin();
-
-  if (costs.at(best_traj_id) != std::numeric_limits<double>::max())
-  {
-    nav_msgs::Path local_path_msg =
-        trajectoryToPathMsg(trajectories.at(best_traj_id));  // use standard path msg for now
-    local_path_pub_.publish(local_path_msg);
-  }
-
-  visualizePaths(trajectories, costs);
+  viz_pub_.publish(*viz_ptr_);
+  viz_ptr_->markers.clear();
 }
 
-void LocalPlanner::globalPathCallback(const nav_msgs::Path& path_msg)
+void LocalPlanner::globalPathCallback(const nav_msgs::Path::ConstPtr& path_msg)
 {
-  global_path_ = path_msg;
+  ref_traj_gen_ptr_->setGlobalPath(path_msg);
 }
 
-void LocalPlanner::odomCallback(const nav_msgs::Odometry& odom_msg)
+void LocalPlanner::odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg)
 {
-  latest_odom_ = odom_msg;
+  latest_odom_ = *odom_msg;
 }
 
-void LocalPlanner::driveCallback(const ackermann_msgs::AckermannDriveStamped& drive_msg)
+void LocalPlanner::driveCallback(const ackermann_msgs::AckermannDriveStamped::ConstPtr& drive_msg)
 {
-  current_steering_angle_ = drive_msg.drive.steering_angle;
+  current_steering_angle_ = drive_msg->drive.steering_angle;
 }
 
 void LocalPlanner::costmapCallback(const grid_map_msgs::GridMap::ConstPtr& costmap_msg)
 {
-  traj_eval_->setCostmap(costmap_msg);
+  ros::Time begin = ros::Time::now();
+
+  collision_checker_ptr_->setCostmap(costmap_msg);
+  inflated_costmap_pub_.publish(collision_checker_ptr_->getInflatedGridMsg());
+
+  ROS_INFO("[Local Planner] CostmapCallback completed in %.3fs", (ros::Time::now() - begin).toSec());
 }
 
-void LocalPlanner::visualizePaths(const std::vector<Trajectory>& trajectories, const std::vector<double>& costs)
+void LocalPlanner::configCallback(local_planner::LocalPlannerConfig& config, uint32_t level)
 {
-  visualization_msgs::MarkerArray trajectory_marker_arr;
+  ref_traj_gen_ptr_->setLengthWeight(config.ref_k_length);
 
-  for (int i = 0; i < trajectories.size(); ++i)
-  {
-    Trajectory trajectory = trajectories.at(i);
+  Lattice::Generator::Pattern lat_gen_pattern(config.ref_num_layers, config.ref_longitudinal_spacing,
+                                              config.ref_num_lateral_samples_per_side, config.ref_lateral_spacing);
+  ref_traj_gen_ptr_->setLatticePattern(lat_gen_pattern);
 
-    visualization_msgs::Marker trajectory_marker;
-    trajectory_marker.header.frame_id = "base_link";
-    trajectory_marker.action = visualization_msgs::Marker::ADD;
-    trajectory_marker.type = visualization_msgs::Marker::LINE_STRIP;
-    trajectory_marker.ns = "waypoints";
-    trajectory_marker.id = i;
-    trajectory_marker.lifetime = ros::Duration(0.1);
-    trajectory_marker.scale.x = 0.02;
-    trajectory_marker.color.r = 0;
-    trajectory_marker.color.g = 1;
-    trajectory_marker.color.b = 0;
-    trajectory_marker.color.a = 1;
+  RTG::VelocityConstraints ref_vel_constraints(config.ref_speed_limit, config.ref_max_lat_acc, config.ref_max_lon_acc,
+                                               config.ref_max_lon_dec);
+  ref_traj_gen_ptr_->setVelocityConstraints(ref_vel_constraints);
 
-    if (costs.at(i) == std::numeric_limits<double>::max())
-    {
-      trajectory_marker.color.r = 1;
-    }
-
-    for (int j = 0; j < trajectory.size(); ++j)
-    {
-      geometry_msgs::Point point;
-      point.x = trajectory.x(j);
-      point.y = trajectory.y(j);
-      trajectory_marker.points.push_back(point);
-
-      visualization_msgs::Marker velocity_marker;
-      velocity_marker.header.frame_id = "base_link";
-      velocity_marker.action = visualization_msgs::Marker::ADD;
-      velocity_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-      velocity_marker.ns = "velocity_profile";
-      velocity_marker.id = i * trajectory.size() + j;
-      velocity_marker.lifetime = ros::Duration(0.1);
-      velocity_marker.scale.z = 0.1;
-      velocity_marker.color.r = 1;
-      velocity_marker.color.g = 1;
-      velocity_marker.color.b = 1;
-      velocity_marker.color.a = 1;
-
-      std::ostringstream oss;
-      oss << std::fixed << std::setprecision(2) << 0;  // temp value
-      velocity_marker.text = oss.str();
-
-      velocity_marker.pose.position = point;
-      velocity_marker.pose.position.z = 0.1;
-      trajectory_marker_arr.markers.push_back(velocity_marker);
-    }
-
-    trajectory_marker_arr.markers.push_back(trajectory_marker);
-  }
-
-  viz_pub_.publish(trajectory_marker_arr);
+  TTG::SamplingPattern track_sampling_pattern(config.track_num_paths, config.track_lateral_spacing,
+                                              config.track_look_ahead_time);
+  track_traj_gen_ptr_->setSamplingPattern(track_sampling_pattern);
 }
-
-int LocalPlanner::getNearestWaypointId()
-{
-  int nearest_wp_id = -1;
-  double nearest_wp_dist = std::numeric_limits<double>::max();
-
-  for (int i = 0; i < global_path_.poses.size(); ++i)
-  {
-    double d_x = latest_odom_.pose.pose.position.x - global_path_.poses.at(i).pose.position.x;
-    double d_y = latest_odom_.pose.pose.position.y - global_path_.poses.at(i).pose.position.y;
-    double dist = sqrt(pow(d_x, 2) + pow(d_y, 2));
-
-    if (dist < nearest_wp_dist)
-    {
-      nearest_wp_id = i;
-      nearest_wp_dist = dist;
-    }
-  }
-
-  return nearest_wp_id;
-}
-
-geometry_msgs::Pose2D LocalPlanner::getReferenceGoal()
-{
-  if (global_path_.poses.empty())
-  {
-    throw std::runtime_error("Global Path is empty");
-  }
-
-  int nearest_wp_id = getNearestWaypointId();
-
-  double path_distance = 0.0;
-  double look_ahead_dist = std::max(look_ahead_time_ * latest_odom_.twist.twist.linear.x, min_look_ahead_dist_);
-  int reference_goal_id = (nearest_wp_id == 0) ? 1 : nearest_wp_id;
-
-  while (reference_goal_id < global_path_.poses.size() && path_distance < look_ahead_dist)
-  {
-    double d_x = global_path_.poses.at(reference_goal_id).pose.position.x -
-                 global_path_.poses.at(reference_goal_id - 1).pose.position.x;
-    double d_y = global_path_.poses.at(reference_goal_id).pose.position.y -
-                 global_path_.poses.at(reference_goal_id - 1).pose.position.y;
-    double dist = sqrt(pow(d_x, 2) + pow(d_y, 2));
-    path_distance += dist;
-    ++reference_goal_id;
-  }
-
-  geometry_msgs::TransformStamped transform =
-      tf_buffer_.lookupTransform(latest_odom_.child_frame_id, global_path_.header.frame_id, ros::Time(0));
-  geometry_msgs::PoseStamped reference_goal_transformed;
-  tf2::doTransform(global_path_.poses.at(reference_goal_id), reference_goal_transformed, transform);
-
-  tf2::Quaternion quat_tf;
-  tf2::fromMsg(reference_goal_transformed.pose.orientation, quat_tf);
-  double roll, pitch, yaw;
-  tf2::Matrix3x3(quat_tf).getEulerYPR(yaw, pitch, roll);
-
-  geometry_msgs::Pose2D reference_goal_2d;
-  reference_goal_2d.x = reference_goal_transformed.pose.position.x;
-  reference_goal_2d.y = reference_goal_transformed.pose.position.y;
-  reference_goal_2d.theta = yaw;
-
-  return reference_goal_2d;
-}
-
-geometry_msgs::Pose2D LocalPlanner::generateOffsetGoal(const geometry_msgs::Pose2D& reference_goal,
-                                                       const double lateral_offset)
-{
-  double x_offset = lateral_offset * cos(reference_goal.theta + M_PI_2);
-  double y_offset = lateral_offset * sin(reference_goal.theta + M_PI_2);
-
-  geometry_msgs::Pose2D goal;
-  goal.x = reference_goal.x + x_offset;
-  goal.y = reference_goal.y + y_offset;
-  goal.theta = reference_goal.theta;
-
-  return goal;
-}
-
-// std::vector<geometry_msgs::Pose2D> LocalPlanner::generateGoals(const geometry_msgs::Pose2D& reference_goal,
-//                                                                const int num_goals, const double lateral_spacing)
-// {
-//   std::vector<geometry_msgs::Pose2D> goals;
-
-//   for (int i = 0; i < num_goals; ++i)
-//   {
-//     double goal_offset = (i - num_goals / 2) * lateral_spacing;
-//     double x_offset = goal_offset * cos(reference_goal.theta + M_PI_2);
-//     double y_offset = goal_offset * sin(reference_goal.theta + M_PI_2);
-
-//     geometry_msgs::Pose2D goal;
-//     goal.x = reference_goal.x + x_offset;
-//     goal.y = reference_goal.y + y_offset;
-//     goal.theta = reference_goal.theta;
-
-//     goals.push_back(goal);
-//   }
-
-//   return goals;
-// }
 
 nav_msgs::Path LocalPlanner::trajectoryToPathMsg(const Trajectory& trajectory)
 {
@@ -318,13 +227,7 @@ nav_msgs::Path LocalPlanner::trajectoryToPathMsg(const Trajectory& trajectory)
   {
     geometry_msgs::PoseStamped pose;
     pose.header.frame_id = trajectory.getFrameId();
-    pose.pose.position.x = trajectory.x(i);
-    pose.pose.position.y = trajectory.y(i);
-
-    tf2::Quaternion yaw_quat;
-    yaw_quat.setRPY(0, 0, trajectory.yaw(i));
-    tf2::convert(yaw_quat, pose.pose.orientation);
-
+    pose.pose = trajectory.pose(i);
     tf2::doTransform(pose, pose, transform);
 
     path_msg.poses.push_back(pose);
