499fb8fdfb1db86a1d5685293f710d478b139e73
Bryan Zheng <ashuh223@gmail.com>
2021-08-28 20:37:51 +0800

    Run library unit tests without roscore

--- local_planner/test/local_planner_tests.cpp
+++ local_planner/test/local_planner_tests.cpp
@@ -78,7 +78,5 @@ TEST(CubicVelocityTimeProfile, invalidArgs)
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
