7347628126aa05dd80f936fd34cb02fcdc47e0b2
Bryan Zheng <ashuh223@gmail.com>
2022-01-25 05:05:36 +0800

    refactor(costmap): Remove inflation service

--- costmap_generator/include/costmap_generator/costmap_generator.h
+++ costmap_generator/include/costmap_generator/costmap_generator.h
@@ -8,8 +8,6 @@
 #include <sensor_msgs/LaserScan.h>
 #include <grid_map_ros/grid_map_ros.hpp>
 
-#include "f1tenth_msgs/InflateCostmap.h"
-
 class CostmapGenerator
 {
 public:
@@ -28,12 +26,11 @@ private:
   grid_map::GridMap global_map_;
   grid_map::GridMap local_map_;
 
-  std::map<std::string, double> client_map_;                   // map client to request
-  std::map<double, std::pair<std::string, int>> request_map_;  // map request to layer_id and number of clients
-  std::map<int, std::string> kernel_map_;                      // map kernel size to layer_id
+  double inflation_radius_;
 
   static const std::string STATIC_LAYER_;
   static const std::string SCAN_LAYER_;
+  static const std::string INFLATION_LAYER_;
 
   double freespace_dist_;  // distance away from obstacles considered to be absolutely free
 
@@ -43,20 +40,11 @@ private:
 
   void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& occ_grid_msg);
 
-  bool
-  serviceCallback(ros::ServiceEvent<f1tenth_msgs::InflateCostmapRequest, f1tenth_msgs::InflateCostmapResponse>& event);
-
-  void addRequest(const std::string& client_id, const double radius, const std::string& layer_id);
-
-  void deleteRequest(const std::string& client_id);
-
   void generateStaticLayer();
 
   void propagateCosts(cv::Mat& input, cv::Mat& output);
 
   void inflateMap(const double radius, const std::string& layer_id);
-
-  static std::string generateLayerId(const double radius);
 };
 
 #endif  // COSTMAP_GENERATOR_COSTMAP_GENERATOR_H
